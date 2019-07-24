670ca696701869ec7f79200d1180add46f0f7177
kizamy <1587545166@qq.com>
2019-07-24 18:47:14 +0200

    lab2

--- catkin_ws/build/cwcw/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "cwcw"
-PROJECT_SPACE_DIR = "/home/user_en/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
