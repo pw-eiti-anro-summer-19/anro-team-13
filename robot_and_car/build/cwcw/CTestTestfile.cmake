34c4b666551520e9c56b1b91e8a21e9d152a142f
cwnevergg <10847666@qq.com>
2019-07-25 16:31:01 +0200

    robot&car

---
+++ robot_and_car/build/cwcw/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user_en/catkin_ws/src/cwcw
+# Build directory: /home/user_en/catkin_ws/build/cwcw
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
