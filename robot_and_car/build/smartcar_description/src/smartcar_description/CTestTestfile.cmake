77812ac30740665a0ff17b6fa7e9cff7f81ce23f
cwnevergg <10847666@qq.com>
2019-07-26 17:24:09 +0200

    update

--- robot_and_car/build/smartcar_description/src/smartcar_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user_en/catkin_ws/src/smartcar_description/src/smartcar_description
-# Build directory: /home/user_en/catkin_ws/build/smartcar_description/src/smartcar_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
